df34416d74a69f0b0ee42148189279f32fe3bb31
Mappel21 <Mappel7676@gmail.com>
2021-07-07 00:54:26 +0000

    creating function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbol = randomSuits();
+  if (symbol === "♥" || symbol === "♦") {
+    document.querySelector("#top").innerHTML = symbol;
+    document.querySelector("#bottom").innerHTML = document.querySelector(
+      "#Top"
+    ).innerHTML;
 };
