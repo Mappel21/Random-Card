de98766f896513d55885febdde69730d516b4264
Mappel21 <Mappel7676@gmail.com>
2021-07-07 01:27:32 +0000

    css

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function () {
+window.onload = function() {
   // assign random suit, color (black is default) and number
   let symbol = randomSuits();
   if (symbol === "♥" || symbol === "♦") {
     document.querySelector("#top").innerHTML = symbol;
-    document.querySelector("#bottom").innerHTML = document.querySelector("#top").innerHTML;
-  };
+    document.querySelector("#bottom").innerHTML = document.querySelector(
+      "#top"
+    ).innerHTML;
     document.querySelector("#top").style.color = "red";
     document.querySelector("#bottom").style.color = "red";
     document.querySelector(".number").innerHTML = randomNumber();
     document.querySelector(".number").style.color = "red";
-  else {
+  } else {
     document.querySelector("#top").innerHTML = symbol;
-    document.querySelector("#bottom").innerHTML = document.querySelector("#top").innerHTML;
+    document.querySelector("#bottom").innerHTML = document.querySelector(
+      "#top"
+    ).innerHTML;
     document.querySelector(".number").innerHTML = randomNumber();
   }
 };
